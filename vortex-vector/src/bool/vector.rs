9583f2063ccb214ee98a46ae3cbe5b5189971465
Connor Tsui <connor.tsui20@gmail.com>
2025-11-26 11:11:41 -0500

    add tests
    Signed-off-by: Connor Tsui <connor.tsui20@gmail.com>

--- vortex-vector/src/bool/vector.rs
+++ vortex-vector/src/bool/vector.rs
@@ -73,6 +73,22 @@ impl BoolVector {
     pub fn bits(&self) -> &BitBuffer {
         &self.bits
     }
+
+    /// Gets a nullable element at the given index, panicking on out-of-bounds.
+    ///
+    /// If the element at the given index is null, returns `None`. Otherwise, returns `Some(x)`,
+    /// where `x: bool`.
+    ///
+    /// Note that this `get` method is different from the standard library [`slice::get`], which
+    /// returns `None` if the index is out of bounds. This method will panic if the index is out of
+    /// bounds, and return `None` if the element is null.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the index is out of bounds.
+    pub fn get(&self, index: usize) -> Option<bool> {
+        self.validity.value(index).then(|| self.bits.value(index))
+    }
 }
 
 impl VectorOps for BoolVector {
